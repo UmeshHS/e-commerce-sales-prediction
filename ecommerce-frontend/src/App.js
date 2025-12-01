fed43e0c58b313c17ac4da0636a2faa8fe52ae80
UmeshHS <148252656+UmeshHS@users.noreply.github.com>
2025-12-01 21:50:08 +0530

    Update App.js

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -25,7 +25,7 @@ export default function DemandPrediction() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("https://e-commerce-sales-prediction-f0ro.onrender.com", {
+      const response = await axios.post("https://e-commerce-sales-prediction-f0ro.onrender.com/predict-prescribe", {
         ...formData,
         product_id: parseInt(formData.product_id),
         store_id: parseInt(formData.store_id),
